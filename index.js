fed8504a3385bb482ddf2e84a365a206fd59a91e
MacBook <sofiiakalamuniak.knm.2019@lpnu.ua>
2022-12-14 02:28:50 +0200

    Make epic 4 task 3

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log("Hello epic 5sad")
-console.log("Make 2 commit right now")
-console.log("Make one more commit right now")
+const http = require('http'); 
+const server = http.createServer(
+    function(request, response) { 
+        response.writeHead(200, { "Content-Type": "text/html" }); 
+        response.end("<html><body><span>SDP EPIC 4</span></body></html>"); 
+    }); 
+const port = process.env.PORT || 1337; 
+server.listen(port); 
+console.log(`Run at http://localhost:${port}`);
